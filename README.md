72dfb970246f77999f6b1779be84a045be6f8cf4
Gran-Dragee <atelier-alice@hotmail.co.jp>
2017-02-28 00:39:44 +0900

    add README and update prepare_osx.sh

---
+++ README.md
@@ -0,0 +1,19 @@
+## dotfiles
+
+```
+$ git clone git@github.com:Gran-Dragee/dotfiles.git
+```
+
+##### OSX
+
+```
+sh prepare_osx.sh
+sh setup.sh
+```
+
+##### Linux(RedHat)
+
+```
+sh prepare_linux.sh
+sh setup.sh
+```
--- prepare_osx.sh
+++ prepare_osx.sh
@@ -10,3 +10,6 @@ brew install ag
 
 brew install rbenv
 brew install ruby-build
+
+brew cask install atom
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
