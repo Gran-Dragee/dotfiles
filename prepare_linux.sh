ddb98a9b664267e2d9e08feed6c0df852c927750
Gran-Dragee <atelier-alice@hotmail.co.jp>
2017-03-07 12:48:59 +0000

    update prepare_linux.sh

--- prepare_linux.sh
+++ prepare_linux.sh
@@ -8,6 +8,7 @@ yum install -y patch
 
 # rbenv
 git clone https://github.com/sstephenson/rbenv.git /usr/local/rbenv
+git clone https://github.com/sstephenson/ruby-build.git /usr/local/rbenv/plugins/ruby-build
 echo 'export RBENV_ROOT="/usr/local/rbenv"' >> ~/.bash_profile
 echo 'PATH=$PATH:$RBENV_ROOT/bin' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
